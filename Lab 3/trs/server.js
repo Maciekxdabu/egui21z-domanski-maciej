83a31b53a682dc5e73bb541cd3d10fe8226ef530
Maciekxdabu <maciek.domanski@onet.pl>
2022-01-27 01:03:47 +0100

    Lab 3 - Displaying of entries

--- Lab 3/trs/server.js
+++ Lab 3/trs/server.js
@@ -1,39 +1,71 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
-const data =
-{
-    "activities":
-    [
-        {
-            "code":"Dev-1",
-            "manager":"jan",
-            "name":"Development",
-            "budget":200
-        },
-        {
-            "code":"DT-2",
-            "manager":"kowal",
-            "name":"Data Mining",
-            "budget":300
-        }
-    ]
-}
+
 app.get('/api/projList', (req, res) => {
     fileName = 'jsons/activity.json';
-    ReadFile(fileName, res);
+    fs.readFile(fileName, (err, out2) => {
+        if (err) throw err;
+        content = JSON.parse(out2);
+        res.json(content);
+    });
+})
+app.get('/api/getReport', (req, res) => {
+    _username = req.query["username"];
+    _date = req.query["date"];
+    ReadReport(_username, _date, res);
 })
 //app.get('/api/projList', (req, res) => res.json(data))
 app.listen(5000)
 
 
+function ReadReport(username, date, res) {
+    var cutDate = date.slice(0, 7);
+    var filename = username + '-' + cutDate + '.json';
+    //var content = ReadFile(path.join(__dirname, 'jsons', filename));
+    fs.readFile(path.join(__dirname, 'jsons', filename), (err, out2) => {
+        if (err) throw err;
+        content = JSON.parse(out2);
 
-function ReadFile(path, res) { // read file from given path and send the contents in res
-    fs.readFile(path, (err, out2) => {
-        if (err) throw err; // if error, throw the error
-        content = JSON.parse(out2); // otherwise parse the binary output into JSON
-        //console.log(content);
-        res.json(content); // send json response ('content' needs to be in json format for this to work)
+        var obj = {};
+        obj.entries = new Array();
+        var index = 0;
+        for (var i = 0; i<content.entries.length; i++) {
+            if (content.entries[i].date != date)
+            {
+                continue;
+            }
+
+            var newObj = {}
+            newObj.code = content.entries[i].code;
+            newObj.time = content.entries[i].time;
+            newObj.description = content.entries[i].description;
+            newObj.date = content.entries[i].date;
+            obj.entries[index] = newObj;
+            index++;
+        }
+        res.send(obj);
+    });
+}
+
+function AddEntry(username, date, newEntry) {
+    //TO DO
+}
+
+function EditEntry(username, date, newEntry) {
+    //TO DO
+}
+
+function DeleteEntry(username, date, newEntry) {
+    //TO DO
+}
+
+function ReadFile(address, res) {
+    fs.readFile(address, (err, out2) => {
+        if (err) throw err;
+        content = JSON.parse(out2);
+        res.json(content);
     });
 }
 
@@ -56,8 +88,27 @@ function WriteFile(name) {
             }
         ]
     }
-    let data = JSON.stringify(content) // change the data structure into a JSON string
+    let data = JSON.stringify(content)
     fs.writeFile(name, data, function (err, result) {
-        if (err) console.log('Error:', err) // if error : log it
+        if (err) console.log('Error:', err)
     })
 }
+
+/*const data =
+{
+    "activities":
+    [
+        {
+            "code":"Dev-1",
+            "manager":"jan",
+            "name":"Development",
+            "budget":200
+        },
+        {
+            "code":"DT-2",
+            "manager":"kowal",
+            "name":"Data Mining",
+            "budget":300
+        }
+    ]
+}*/
--- Lab 3/trs/src/Pages/MainWindow.js
+++ Lab 3/trs/src/Pages/MainWindow.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function MainWindow() {
+function MainWindow(props) {
+    const [entryList, setEntryList] = useState({ entries: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const config = {
+                method: 'get',
+                url: '/api/getReport',
+                params:{
+                    username: props.uName,
+                    date: props.uDate
+                }
+            }
+            const result = await axios(config);
+
+            console.log(result.data);
+            setEntryList(result.data)
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             <h2>
                 MainWindow Page
             </h2>
+            <table>
+                <tr>
+                    <th>
+                        Project Code
+                    </th>
+                    <th>
+                        Time Spent
+                    </th>
+                    <th>
+                        Description
+                    </th>
+                </tr>
+                {entryList.entries.map(item => (
+                <tr>
+                    <td>
+                        {item.code}
+                    </td>
+                    <td>
+                        {item.time}
+                    </td>
+                    <td>
+                        {item.description}
+                    </td>
+                </tr>
+                ))}
+            </table>
         </div>
     );
 };
